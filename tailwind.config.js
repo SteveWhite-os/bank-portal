c43684e3e877f5c5056a299fa7d399460c3bfe21
Black Box <supsam40@gmail.com>
2026-07-04 06:58:43 -0700

    fix: clean config files with correct JS

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,3 @@
-cat > tailwind.config.js << 'EOF'
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
@@ -9,5 +8,4 @@ module.exports = {
     extend: {},
   },
   plugins: [],
-}
-EOF
+};
